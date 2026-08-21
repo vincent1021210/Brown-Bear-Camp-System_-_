8a7869527fbcb68b7d9993fcd328bedde713b549
vincent1021210 <vincent1021210@users.noreply.github.com>
2026-08-21 01:01:40 +0000

    deploy: 192dddb17430612266141396e3dc74dccd45ee3d

---
+++ _next/static/lTt7TFxhkaH6lwLvmpz8u/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Brown-Bear-Camp-System_-_//_next/:path+",
+        "destination": "/Brown-Bear-Camp-System_-_/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
---
+++ _next/static/lTt7TFxhkaH6lwLvmpz8u/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fteam\u002F[teamId]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
